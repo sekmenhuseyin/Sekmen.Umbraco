40927265fb388fdd26310d8745cceca4639ea7f6
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2020-08-18 18:20:17 +0300

    upload

---
+++ CreatorCms.Core/Controllers/UploadController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+// ReSharper disable Mvc.ViewNotResolved
+
+namespace CreatorCms.Core.Controllers
+{
+    public class UploadController : SurfaceController
+    {
+        [HttpPost, ValidateAntiForgeryToken]
+        public JsonResult Document(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+
+            try
+            {
+                string mediaTypeAlias = file.ContentType.Contains("image") ? "Image" : "File";
+                IMedia media = Services.MediaService.CreateMedia(file.FileName, 1054, mediaTypeAlias);
+                media.SetValue("umbracoFile", file);
+                Services.MediaService.Save(media);
+
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Logger.Error<UploadController>("Document", e);
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ Tanman.Umbraco/Js/main.js
@@ -0,0 +1,30 @@
+﻿// ReSharper disable IdentifierTypo
+
+$(document).ready(function () {
+    window.Dropzone.options.dropzoneForm = {
+        addRemoveLinks: true,
+        init: function () {
+            this.on("success", function (data) {
+                if (data === true) {
+                    window.toastr["success"]("Dosya yüklendi", "Başarılı");
+                } else {
+                    window.toastr["error"]("Dosya yüklenemedi", "Hata");
+                }
+            });
+            this.on("error", function (data) {
+                console.log(data);
+                window.toastr["error"]("Dosya yüklenemedi", "Hata");
+            });
+            this.on("complete", function (file) { this.removeFile(file); });
+            this.on("canceled", function (file) { this.removeFile(file); });
+        },
+        uploadprogress: function (file, progress, bytesSent) {
+            if (file.previewElement) {
+                var progressElement = file.previewElement.querySelector("[data-dz-uploadprogress]");
+                progressElement.style.width = progress + "%";
+                progressElement.querySelector(".progress-text").textContent = progress + "%";
+            }
+        }
+    };
+
+});
