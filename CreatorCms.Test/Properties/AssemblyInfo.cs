af58574d947a63c5766f26e10f427c1fccd0888c
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2020-08-31 16:10:04 +0300

    test project

---
+++ CreatorCms.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CreatorCms.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreatorCms.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("617631bc-b3b8-49f8-b5a9-742d7de73128")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatorCms.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace CreatorCms.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
