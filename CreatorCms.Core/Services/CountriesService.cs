ca2d355bfd228322c0554b8eceef9f3411ca36eb
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2020-08-31 16:09:58 +0300

    countries api

---
+++ CreatorCms.Core/Services/CountriesService.cs
@@ -0,0 +1,24 @@
+﻿using CreatorCms.Core.Services.Models;
+using RestSharp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CreatorCms.Core.Services
+{
+    public class CountriesService// : UmbracoApiController
+    {
+        private const string RapidapiHost = "restcountries-v1.p.rapidapi.com";
+        private const string RapidapiKey = "75d4248cc5msh98b980e86f79fe8p1d703fjsn9569090dd601";
+
+        public async Task<List<CountryResponse>> GetAllCountries()
+        {
+            RestClient client = new RestClient("https://restcountries-v1.p.rapidapi.com/all");
+            RestRequest request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", RapidapiHost);
+            request.AddHeader("x-rapidapi-key", RapidapiKey);
+            List<CountryResponse> response = await client.GetAsync<List<CountryResponse>>(request);
+
+            return response;
+        }
+    }
+}
---
+++ CreatorCms.Core/Services/Models/CountryResponse.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+
+namespace CreatorCms.Core.Services.Models
+{
+    public class CountryResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("topLevelDomain")]
+        public string[] TopLevelDomain { get; set; }
+
+        [JsonProperty("alpha2Code")]
+        public string Alpha2Code { get; set; }
+
+        [JsonProperty("alpha3Code")]
+        public string Alpha3Code { get; set; }
+
+        [JsonProperty("callingCodes")]
+        public string[] CallingCodes { get; set; }
+
+        [JsonProperty("capital")]
+        public string Capital { get; set; }
+
+        [JsonProperty("altSpellings")]
+        public string[] AltSpellings { get; set; }
+
+        [JsonProperty("region")]
+        public Region Region { get; set; }
+
+        [JsonProperty("subregion")]
+        public string Subregion { get; set; }
+
+        [JsonProperty("population")]
+        public long Population { get; set; }
+
+        [JsonProperty("latlng")]
+        public double[] Latlng { get; set; }
+
+        [JsonProperty("demonym")]
+        public string Demonym { get; set; }
+
+        [JsonProperty("area")]
+        public double? Area { get; set; }
+
+        [JsonProperty("gini")]
+        public double? Gini { get; set; }
+
+        [JsonProperty("timezones")]
+        public string[] Timezones { get; set; }
+
+        [JsonProperty("borders")]
+        public string[] Borders { get; set; }
+
+        [JsonProperty("nativeName")]
+        public string NativeName { get; set; }
+
+        [JsonProperty("numericCode")]
+        public string NumericCode { get; set; }
+
+        [JsonProperty("currencies")]
+        public string[] Currencies { get; set; }
+
+        [JsonProperty("languages")]
+        public string[] Languages { get; set; }
+
+        [JsonProperty("translations")]
+        public Translations Translations { get; set; }
+
+        [JsonProperty("relevance")]
+        public string Relevance { get; set; }
+    }
+
+    public class Translations
+    {
+        [JsonProperty("de")]
+        public string De { get; set; }
+
+        [JsonProperty("es")]
+        public string Es { get; set; }
+
+        [JsonProperty("fr")]
+        public string Fr { get; set; }
+
+        [JsonProperty("ja")]
+        public string Ja { get; set; }
+
+        [JsonProperty("it")]
+        public string It { get; set; }
+    }
+    public enum Region { Africa, Americas, Asia, Empty, Europe, Oceania, Polar };
+}
