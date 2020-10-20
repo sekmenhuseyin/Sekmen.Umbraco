5b706aeb4a0a56c00f4d6a18240fb35ad9492010
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2020-10-20 14:52:21 +0300

    web project

---
+++ Umbraco.Web/Js/main.js
@@ -0,0 +1,20 @@
+﻿// ReSharper disable IdentifierTypo
+window.Dropzone.options.dropzoneForm = {
+    addRemoveLinks: true,
+    init: function () {
+        this.on("success", function (data) {
+            if (data === true) {
+                window.toastr["success"]("Dosya yüklendi", "Başarılı");
+            } else {
+                window.toastr["error"]("Dosya yüklenemedi", "Hata");
+                $(".dz-error").remove();
+            }
+        });
+        this.on("error", function (data) {
+            window.toastr["error"]("Dosya yüklenemedi", "Hata");
+            $(".dz-error").remove();
+        });
+        this.on("complete", function (file) { this.removeFile(file); });
+        this.on("canceled", function (file) { this.removeFile(file); });
+    }
+};
